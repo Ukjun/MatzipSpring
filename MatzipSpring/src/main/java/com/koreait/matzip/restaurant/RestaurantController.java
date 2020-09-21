833c86c5ffae97996afa2f2bca345adce5f8cc7f
Administrator <Administrator@DESKTOP-ISVFPRU>
2020-09-21 12:15:11 +0900

    restaurant초기설정

--- MatzipSpring/src/main/java/com/koreait/matzip/restaurant/RestaurantController.java
+++ MatzipSpring/src/main/java/com/koreait/matzip/restaurant/RestaurantController.java
@@ -1,14 +1,20 @@
 package com.koreait.matzip.restaurant;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.koreait.matzip.Const;
+import com.koreait.matzip.ViewRef;
 
 @Controller
 @RequestMapping("/restaurant")
 public class RestaurantController {
-	@RequestMapping(value = "/map", method = RequestMethod.GET)
-	public String map() {
-		return "restaurant/map";
+	@RequestMapping("/map")
+	public String map(Model model) {
+		model.addAttribute(Const.TITLE,"Map");
+		model.addAttribute(Const.VIEW, "restaurant/map");
+		return ViewRef.TEMP_MAP;
 	}
 }
